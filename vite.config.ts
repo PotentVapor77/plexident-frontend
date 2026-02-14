dc3ab723b7c37782e06d6b474fe9ce1620e6f821
NavaCorp26 <luisnavarrete1a89@gmail.com>
2026-02-14 17:27:13 -0500

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,31 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-    ,
+  plugins: [
+    react(),
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
     }),
   ],
 
+  envPrefix: 'VITE_',
+
   server: {
     watch: {
       usePolling: true,
     },
-    host: true, 
+    host: true,
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-  
 })

cbee1c5ff0c4a0ffc4af7f61d88d3ed1ec18e5bc
NavaCorp26 <luisnavarrete1a89@gmail.com>
2026-02-14 22:57:38 -0500

    modified:   src/pages/AuthPages/AuthPageLayout.tsx

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -16,28 +16,7 @@ export default function AuthLayout({
             {children}
           </div>
 
-          {/* Footer Links */}
-          <div className="mt-6 text-center">
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              ¿No tienes una cuenta?{" "}
-              <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                Contáctanos
-              </a>
-            </p>
-            
-            <div className="mt-4 text-xs text-gray-500 dark:text-gray-400">
-              <p>Copyright © 2025 Plexident. Todos los derechos reservados.</p>
-              <div className="mt-2 space-x-4">
-                <a href="#" className="hover:text-gray-700 dark:hover:text-gray-300">
-                  Términos de Servicio
-                </a>
-                <a href="#" className="hover:text-gray-700 dark:hover:text-gray-300">
-                  Política de Privacidad
-                </a>
-              </div>
-            </div>
           </div>
-        </div>
       </div>
 
       {/* Mitad derecha - Imagen que cubre TODO el espacio */}
